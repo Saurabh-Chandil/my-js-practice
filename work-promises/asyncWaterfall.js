e17a69ff76cd69ee87158d649a45699a45e98af0
Saurabh Chandil <saurabh.chandil2013@gmail.com>
2020-02-15 12:57:32 +0530

    async waterfall

---
+++ work-promises/asyncWaterfall.js
@@ -0,0 +1,27 @@
+var async =  require('async');
+async.waterfall([
+  (done) => {
+    console.log('start!');
+
+    done(null, 'Value from step 1'); // <- set value to passed to step 2
+  },
+  (step1Result, done) =>{
+    console.log(step1Result);
+    console.log('Second step');
+    //done(null, 'Value from step 2'); // <- set value to passed to step 3
+    return done(null);
+  },
+  (step2Result, done) => {
+    console.log(step2Result);
+    console.log('Third step');
+    //return done('Breaked'); // <- no value set for the next step.
+  }
+],
+(err) => {
+  if (err) {
+    console.log('err:', err);  
+    //throw new Error(err);
+  } else {
+    console.log('No error happened in any steps, operation done!');
+  }
+});
---
+++ work-promises/breakPromisesChain.js
@@ -0,0 +1,87 @@
+// Understanding promises
+
+function gatherHardware() {
+    console.log('Gather hadware');
+    return new Promise((resolve, reject) => {
+        resolve('MacParts');
+    });
+}
+
+function gatherSofwareInstallers(hadware) {
+    console.log('Arrange software for gathered : ', hadware);
+    return new Promise((resolve, reject) => {
+        resolve('MacPartsAndsw');
+    });
+}
+
+function assembleHardware(baseMachine) {
+    console.log('Assembly Line. We got : ', baseMachine);
+    return new Promise((resolve, reject) => {
+        //resolve('assembledParts');
+        reject('AssemblyNotWorking');
+    });
+}
+
+function installSoftware(assembledMachine) {
+    console.log('Install software in : ', assembledMachine);
+    return new Promise((resolve, reject) => {
+        resolve('completeMachine');
+    });
+}
+
+let laptop = () => {
+    return gatherHardware().then((resOne) => {
+        console.log('First then : ', resOne);
+        return gatherSofwareInstallers(resOne);
+    }).then((resTwo) => {
+        console.log('Second then : ', resTwo);
+        return assembleHardware(resTwo);
+    }).then((resThree) => {
+        console.log('Third then : ', resThree);
+        return installSoftware(resThree);
+    }).catch((err) => {
+        console.log('Machine cant be completed : ', err);
+        return 'Incomplete machine';
+    });
+}
+
+// laptop().then((finalResponse) => {
+//     console.log('Final product : ', finalResponse);
+// });
+
+
+// ************************* Now breaking the chain *************************
+
+// ####################### Not able to fully understand this concept. For more clarification goto https://stackoverflow.com/questions/32032588/how-to-return-from-a-promises-catch-then-block
+function functionOne() {
+    console.log('One');
+    //throw new Error('breaking');
+    return new Promise((resolve, reject) => {
+        //resolve('ONE');
+        reject('NOOO');
+    });
+}
+function functionTwo() {
+    console.log('Two');
+    return new Promise((resolve, reject) => {
+        resolve('TWO');
+    });
+}
+function functionThree() {
+    console.log('Three');
+    return new Promise((resolve, reject) => {
+        resolve('THREE');
+    });
+}
+
+try {
+    Promise.resolve()
+    .then(functionOne()).catch((funcOneErr) => {
+        console.log('First catch');
+    }).then(functionTwo()).catch((funcTwoErr) => {
+        console.log('Second catch');
+    });
+
+} catch (error) {
+    console.log('My problem : ', error);
+}
